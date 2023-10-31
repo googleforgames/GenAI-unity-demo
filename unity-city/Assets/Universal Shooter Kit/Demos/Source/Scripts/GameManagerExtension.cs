1edc4e8d8756479d3b4c63ab89ab7b49be5b69fc
Serge Shautsou <serge.shautsou@globant.com>
2023-10-31 13:05:13 -0300

    ChatBox UI implemented

--- unity-city/Assets/Universal Shooter Kit/Demos/Source/Scripts/GameManagerExtension.cs
+++ unity-city/Assets/Universal Shooter Kit/Demos/Source/Scripts/GameManagerExtension.cs
@@ -7,7 +7,8 @@ public class GameManagerExtension : MonoBehaviour
 {
     [SerializeField] private List<InteractableObject> _interactableObjects = new List<InteractableObject>();
     // TODO: move to the UIManager
-    [SerializeField] private TextMeshProUGUI _uiHint;
+    [SerializeField] private TextMeshProUGUI _interactionCallout;
+    [SerializeField] private ChatBoxController _chatBoxController;
     private bool _isHintShown = false;
 
     private void Start()
@@ -29,9 +30,9 @@ public class GameManagerExtension : MonoBehaviour
         }
 
         _isHintShown = showHint;
-        if (_uiHint)
+        if (_interactionCallout)
         {
-            _uiHint.gameObject.SetActive(showHint);
+            _interactionCallout.gameObject.SetActive(showHint);
         }
     }
 }
