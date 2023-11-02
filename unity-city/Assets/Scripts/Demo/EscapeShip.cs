34822d61757ab1df71fe5e34d03bf62429bdd9bb
Jesus Gonzalez <82907841+jesus-gonzalez-globant@users.noreply.github.com>
2023-11-02 09:16:35 -0300

    Spaceship animation (#13)
    * Animate escape ship flying away.
    
    * Make ship fly further away.
    
    ---------
    
    Co-authored-by: Jesus Gonzalez <jesus@jesusmg.net>

---
+++ unity-city/Assets/Scripts/Demo/EscapeShip.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Demo
+{
+    public class EscapeShip : NetworkBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        
+        private static readonly int FlyAwayTriggerHash = Animator.StringToHash("FlyAway");
+
+        public void FlyAway()
+        {
+            _animator.SetTrigger(FlyAwayTriggerHash);
+        }
+    }
+}
