92211e24d709b36ab4b2b51004b0c6978d99037b
Jesus Gonzalez <jesusmaria.gonzalez@globant.com>
2023-11-21 11:13:25 -0300

    Keep only relevant files

---
+++ Assets/FreeOpenBuilding_1.1.2/FOB_1/Scripts/DoorMech.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMech : MonoBehaviour 
+{
+
+	public Vector3 OpenRotation, CloseRotation;
+
+	public float rotSpeed = 1f;
+
+	public bool doorBool;
+
+
+	void Start()
+	{
+		doorBool = false;
+	}
+		
+	void OnTriggerStay(Collider col)
+	{
+		if(col.gameObject.tag == ("Player") && Input.GetKeyDown(KeyCode.E))
+		{
+			if (!doorBool)
+				doorBool = true;
+			else
+				doorBool = false;
+		}
+	}
+
+	void Update()
+	{
+		if (doorBool)
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (OpenRotation), rotSpeed * Time.deltaTime);
+		else
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (CloseRotation), rotSpeed * Time.deltaTime);	
+	}
+
+}
+
